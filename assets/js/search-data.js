1e6bd4c900432fdb5f2f9fbc69a9d4aca46393fd
zhenwu0728 <zhenwu@mit.edu>
2025-03-27 05:28:31 +0000

    Deploying to gh-pages from @ zhenwu0728/zhenwu0728.github.io@7d9affa6549756351316318b93158092275ab581 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,19 +9,19 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-research",
+          title: "research",
+          description: "Understanding the interactions between microbial activities and biogeochemical cycles",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "# Contributed equally * Corresponding author",
           section: "Navigation",
           handler: () => {
             window.location.href = "/publications/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
           },
         },{id: "nav-people",
           title: "people",
@@ -30,29 +30,14 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/people/";
           },
-        },{id: "post-google-gemini-updates-flash-1-5-gemma-2-and-project-astra",
-        
-          title: 'Google Gemini updates: Flash 1.5, Gemma 2 and Project Astra <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "We’re sharing updates across our Gemini family of models and a glimpse of Project Astra, our vision for the future of AI assistants.",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://blog.google/technology/ai/google-gemini-update-flash-ai-assistant-io-2024/", "_blank");
-          
-        },
-      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
-        
-          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
-        
-        description: "",
-        section: "Posts",
-        handler: () => {
-          
-            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
-          
-        },
-      },{id: "news-tada-our-paper-planktonindividuals-jl-a-gpu-supported-individual-based-phytoplankton-life-cycle-model-is-published-in-journal-of-open-source-software",
+        },{id: "nav-cv",
+          title: "cv",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/cv/";
+          },
+        },{id: "news-tada-our-paper-planktonindividuals-jl-a-gpu-supported-individual-based-phytoplankton-life-cycle-model-is-published-in-journal-of-open-source-software",
           title: ':tada: Our paper PlanktonIndividuals.jl: A GPU supported individual-based phytoplankton life cycle model is...',
           description: "",
           section: "News",},{id: "news-tada-our-paper-imbalance-of-global-nutrient-cycles-exacerbated-by-the-greater-retention-of-phosphorus-over-nitrogen-in-lakes-is-published-in-nature-geoscience",
@@ -64,19 +49,27 @@ ninja.data = [{
           section: "News",},{id: "news-tada-i-started-a-new-position-as-an-assistant-professor-at-peking-university-and-now-looking-for-postdocs-and-phd-students",
           title: ':tada: I started a new position as an Assistant Professor at Peking University,...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "research-planktonindividuals-jl",
+          title: 'PlanktonIndividuals.jl',
+          description: "Development of Individual-Based Micobial Life Cycle Model",
+          section: "Research",handler: () => {
+              window.location.href = "/research/1_project/";
+            },},{id: "research-intracellular-carbon-flux",
+          title: 'Intracellular Carbon Flux',
+          description: "Phtosynthesis, exudation, and mixotrophy etc. in different environment",
+          section: "Research",handler: () => {
+              window.location.href = "/research/2_project/";
+            },},{id: "research-nutrient-retention-in-global-lakes",
+          title: 'Nutrient Retention in Global Lakes',
+          description: "Resolving Nitrogen and Phosphorus Cycles in Global Lakes",
+          section: "Research",handler: () => {
+              window.location.href = "/research/3_project/";
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
           window.open("mailto:%7A%68%65%6E%77%75@%70%6B%75.%65%64%75.%63%6E", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
         },
       },{
         id: 'social-scholar',
